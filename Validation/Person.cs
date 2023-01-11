263226a224ee536822935f51cc96a7a72c92c417
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2023-01-11 11:21:42 +0100

    Validation

---
+++ Validation/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validation
+{
+    internal class Person : IDataErrorInfo
+    {
+		private string name;
+        public string Name
+		{
+			get { return name; }
+			set 
+			{
+				if (value.All(x => Char.IsLetter(x)))
+					name = value;
+				else
+					throw new Exception("Bitte gib nur Buchstaben ein.");
+			}
+		}
+
+		public int Alter { get; set; }
+
+
+		public string Error => null;
+        public string this[string columnName]
+		{
+			get
+			{
+				switch (columnName)
+				{
+					case (nameof(Alter)):
+						if (Alter < 0 || Alter > 150) return "Bitte gib das wahre Alter an.";
+						break;
+
+					default:
+						break;
+				}
+
+				return null;
+			}
+		}
+    }
+}
