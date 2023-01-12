9ee2d9c3feb8b58a5de8c364b417cb03619170fd
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2023-01-12 16:30:18 +0100

    final

---
+++ Multithreading/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Multithreading
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public int Counter { get; set; } = 0;
+
+        private void Btn_ShortTask_Click(object sender, RoutedEventArgs e)
+        {
+            Counter += 1;
+            Tbl_ShortTask.Text = Counter.ToString();
+        }
+
+        private async void Btn_LongTask_Click(object sender, RoutedEventArgs e)
+        {
+            string taskResult = "";
+
+            taskResult = await Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+                return "Fertig";
+            });
+
+            Tbl_LongTask.Text = taskResult;
+
+        }
+    }
+}
